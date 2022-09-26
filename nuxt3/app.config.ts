83bfa9e284a9a8a1c719cfe8a48404971f9b834b
Maksmm <maks_minakov@icloud.com>
2022-09-26 18:03:15 +0300

    deleted folder

--- nuxt3/app.config.ts
+++
@@ -1,4 +0,0 @@
-export default defineAppConfig({
-	apiKey: "c7a8db5d7a285db046903fd598d93e08",
-	ombdToken: "b0333a53",
-});
--- nuxt3/pages/list.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div>
-		test
-	</div>
-</template>
-
-<style lang="scss" scoped>
-	
-</style>
